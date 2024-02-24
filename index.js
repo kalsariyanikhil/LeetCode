4da178bc032c277c004906f423dd9b9b9eb3ae8c
nikhil kalsariya <nickahir0786@gmail.com>
2024-02-24 08:50:43 +0530

    basic

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// power
+
+
 function math(x,n){
-    return Math.pow(x,n);
+    return (Math.pow(x,n));
 }
-console.log(math(2.00000,10))
+console.log(math(2,10))
+
+
+
+// shorting-75
+
+var sortColors = function(nums) {
+    console.log(nums.sort((a,b)=>a-b));
+};
+console.log(sortColors([2,0,222,1,1,0]));
+
